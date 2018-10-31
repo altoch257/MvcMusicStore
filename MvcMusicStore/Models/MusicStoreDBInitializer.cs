b4129b807b68531ef27a7166d3e5ee245e51bb0f
Jean Petit <altochp@gmail.com>
2018-10-31 17:06:23 -0400

    Added MusicStoreDBInitializer class to create dummy data

---
+++ MvcMusicStore/Models/MusicStoreDBInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+
+    public class MusicStoreDBInitializer : DropCreateDatabaseAlways<MusicStoreDB>
+    {
+        protected override void Seed(MusicStoreDB context)
+        {
+            context.Artists.Add(new Artist { Name = "Donnie McClurkin" });
+            context.Genres.Add(new Genre { Name = "Gospel" });
+            context.Albums.Add(new Album
+            {
+                Artist = new Artist { Name = "Ray Charles" },
+                Genre = new Genre { Name = "Soul" },
+                Price = 9.99m,
+                Title = "Genius Loves Company"
+            });
+
+            base.Seed(context);
+        }
+    }
+}
